4ee9e2ca0de483139c33066eb6f39282bce443f8
debikusikus <99951442+debikusikus@users.noreply.github.com>
2022-12-07 21:56:53 +0700

    Add files via upload

---
+++ Kuzin_403_group_java/Lab4/2.java
@@ -0,0 +1,20 @@
+﻿/* KuzinD */
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter m: ");
+		int m = input.nextInt();
+		double res = 0, n = 1;
+		for (int i = 1; i <= m; i++) {
+			if (i%2 == 0) {
+				res -= n/i;
+			}
+			else {
+				res += n/i;
+			}
+		} 
+		System.out.print("Result: " + res);
+	}
+}
